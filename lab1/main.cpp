e9978ff84ca5a114af7f8c3d213bbf6eb3c54e46
BobTailer <86107256+BobTailer@users.noreply.github.com>
2024-02-21 18:12:27 +0300

    edited main.cpp with included libs

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
-#include <cstring>
+#include "mymath/mymath.h"
+#include "time_lib/time_lib.h"
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    // сюда решение
+
+    //Библиотека mymath
+    std::cout << "Я умею суммировать! Зацени 1+1=";
+    std::cout << mymath::sum(10, 32) << std::endl;
+
+    std::cout << "Я умею считать время! Зацени\n";
+    time_lib::time_calc(17, 5, 5, 59);
+
+
     return 0;
 }
